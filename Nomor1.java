6ef5ac849e138d584e47c21f823119fdc5cd89ff
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-12-12 12:49:27 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-        FizzBuzz(N);
-    }
-        public static void FizzBuzz(int N) {
-        for (int i = 0; i <= N; i++) {
+    // Prosedur untuk menampilkan deret FizzBuzz
+    public static void prosesOutputFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            // Cek kelipatan 3 dan 5 terlebih dahulu
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
             } 
+            // Cek kelipatan 3
             else if (i % 3 == 0) {
                 System.out.print("Fizz ");
             } 
+            // Cek kelipatan 5
             else if (i % 5 == 0) {
                 System.out.print("Buzz ");
             } 
+            // Jika bukan kelipatan 3 atau 5, cetak bilangan
             else {
                 System.out.print(i + " ");
             }
         }
-        System.out.println();
-        /* Pekerjaan anda berakhir sisi */
+    }
 
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan nilai N : ");
+        int N = input.nextInt();
+        
+        /* Pekerjaan anda mulai dari sini */
+        prosesOutputFizzBuzz(N);
+        /* Pekerjaan anda berakhir sisi */
+        
         System.out.print(Nomor1.class.getDeclaredMethods().length); 
     }
 }
