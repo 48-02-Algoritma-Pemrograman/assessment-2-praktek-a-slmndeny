1c0386f6e31e53c0e0fe7245c4b7377b24078b73
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-12-12 12:15:38 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,14 +5,20 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-        hitungGanjil(N);
+        printFizzBuzz(N);
     }
-        public static void hitungGanjil(int N){
-            for(int i =0; i <= N; i++){
-                if (i % 2 ==1) {
-                    System.out.println(i);
-                }
+    public static void printFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
             }
+        }
         /* Pekerjaan anda berakhir sisi */
 
         System.out.print(Nomor1.class.getDeclaredMethods().length); 
