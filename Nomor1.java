41ba991f39edb4aa23c34a3c272553a278ee4013
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-12-12 12:33:10 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,24 +3,27 @@ import java.util.Scanner;
 public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukan nilai N : ");
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-       printFizzBuzz(N);
+        printFizzBuzz(N);
+        System.out.println();
+        System.out.print(Nomor1.class.getDeclaredMethods().length);
     }
-    public static void printFizzBuzz(int n) {
-        for (int i = 0; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.print("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.print("Buzz");
-            } else {
-                System.out.print(i);
+        public static void printFizzBuzz(int n) {
+            for (int i = 0; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.print("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.print("Buzz");
+                } else {
+                    System.out.print(i);
+                }
+                if (i < n) {
+                    System.out.print(" ");
+                }
             }
-        }
-        /* Pekerjaan anda berakhir sisi */
-
-        System.out.print(Nomor1.class.getDeclaredMethods().length); 
     }
 }
