b0352d402e559fad8a944ca7d6a6ee595d460c01
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-12-12 12:27:29 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,24 +3,20 @@ import java.util.Scanner;
 public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukan nilai N : ");
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-        FizzBuzz(N);
+       printFizzBuzz(N);
     }
-    public static void FizzBuzz(int n) {
+    public static void printFizzBuzz(int n) {
         for (int i = 0; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz ");
-            } 
-            else if (i % 3 == 0) {
-                System.out.print("Fizz ");
-            } 
-            else if (i % 5 == 0) {
-                System.out.print("Buzz ");
-            } 
-            else {
-                System.out.print(i + " ");
+                System.out.print("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz");
+            } else {
+                System.out.print(i);
             }
         }
         /* Pekerjaan anda berakhir sisi */
